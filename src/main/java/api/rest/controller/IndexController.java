401f7895230bb0f8ca2c713d4dff8b967d5a12ea
maximilianocaetano <maximilianocaetano@gmail.com>
2021-08-11 16:36:38 -0300

    update user

--- src/main/java/api/rest/controller/IndexController.java
+++ src/main/java/api/rest/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,7 +79,23 @@ public class IndexController {
 		
 		//Usuario usuarioSalvo = usuarioRepository.save(usuario);		
 		
-		return new ResponseEntity("id user: " +iduser+ " idvenda: "+idvenda,HttpStatus.OK);
+		return new ResponseEntity("id user: " +iduser+ " idvenda: "+idvenda,HttpStatus.OK);		
+	}
+	
+	@PutMapping(value = "/",produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
 		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);		
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo,HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value = "/{iduser}/idvenda/{idvenda}",produces = "application/json")
+	public ResponseEntity updateVenda(@PathVariable Long iduser,@PathVariable Long idvenda){
+		
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);		
+		
+		return new ResponseEntity("Venda atualizada",HttpStatus.OK);		
 	}
 }
